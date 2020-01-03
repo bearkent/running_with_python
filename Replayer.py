0c775ed4fae5545e6ee90ad0c79795a0b5bb7247
Bear Kent <davidbearkent@gmail.com>
2020-01-03 11:59:31 -0700

    added LED signals and joystick start and end controls.

--- Replayer.py
+++ Replayer.py
@@ -3,12 +3,21 @@ import time
 import csv
 
 def record(file):
+    red=(255,0,0)
+    white=(255,255,255)
     sense = SenseHat()
     acceleration = sense.get_accelerometer_raw()
+
+    sense.show_letter("W",text_colour=red,back_colour=white)
+    sense.stick.wait_for_event()
+
+    sense.show_letter("R",text_colour=red,back_colour=white)
+    
 
     with open(file,"w") as f:
         writer = csv.writer(f)
         while True:
+        
             t=time.time_ns()
             o = sense.get_orientation()
             ax = o["pitch"]
@@ -18,6 +27,14 @@ def record(file):
             y = acceleration['y']
             z = acceleration['z']
             writer.writerow([t,ax,ay,az,x,y,z])
+
+            events = sense.stick.get_events()
+
+            for event in events:
+                if event.action == "pressed":
+                    sense.clear()
+                    return
+
 
 def replay(file, fx):
     with open(file,"r") as f:
